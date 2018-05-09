37c2904aefb9915d5bc1f2d6c44f3018fa99c9c7
nilbaro <nil.baro@enti.cat>
2018-05-09 13:21:07 +0200

    nombres cambiados

--- Mindblow/Assets/scripts/GameControl.cs
+++ Mindblow/Assets/scripts/GameControl.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 public class GameControl : MonoBehaviour
 {
 
-    public bool llave = false, habitacio1 = false;
+    public bool llave = false, habitacio1 = false, luz = false;
     public GameObject jugador;
     public Transform transformJugador;
     public GameObject Llave;
     public GameObject Llave2;
     public GameObject Llave3;
+    public GameObject Llave4;
+    public GameObject Luz;
     public GameObject Trampilla;
     public GameObject puertaLlave;
     public GameObject puertaLlave2;
     public GameObject puertaLlave3;
+    public GameObject puertaLlave4;
     public float lastPosX, lastPosY;
-    public float xProva1, yProva1, xProva2, yProva2, nivel2X, nivel2Y, xProva3, yProva3;
-    public float xPos1, yPos1, xPos2, yPos2, xPos3, yPos3, xPos4, yPos4, xPos5, yPos5, xPos6, yPos6, xPos7, yPos7, xPos8, yPos8, xPos9, yPos9, xPos10, yPos10, xPos11, yPos11, xPos12, yPos12, xPos13, yPos13, xPos14, yPos14;
-    public float x1PL1, y1PL1, x2PL1, y2PL1, x1PL2, y1PL2, x2PL2, y2PL2, x1PL3, y1PL3, x2PL3, y2PL3;
+    public float xProva1, yProva1, xProva2, yProva2, nivel2X, nivel2Y, xProva3, yProva3, xProva4, yProva4, xProva5, yProva5;
+    public float xPos1, yPos1, xPos2, yPos2, xPos3, yPos3, xPos4, yPos4, xPos5, yPos5, xPos6, yPos6, xPos7, yPos7, xPos8, yPos8, xPos9, yPos9, xPos10, yPos10, xPos11, yPos11, xPos12, yPos12, xPos13, yPos13, xPos14, yPos14, xPos15, yPos15, xPos16, yPos16, xPos17, yPos17, xPos18, yPos18, xPos19, yPos19, xPos20, yPos20, xPos21, yPos21, xPos22, yPos22;
+    public float x1PL1, y1PL1, x2PL1, y2PL1, x1PL2, y1PL2, x2PL2, y2PL2, x1PL3, y1PL3, x2PL3, y2PL3, x1PL4, y1PL4, x2PL4, y2PL4;
     public float tx1, tx2, ty1, ty2;
 
     private void Awake()
@@ -28,9 +31,12 @@ public class GameControl : MonoBehaviour
         Llave = GameObject.FindGameObjectWithTag("Llave");
         Llave2 = GameObject.FindGameObjectWithTag("Llave2");
         Llave3 = GameObject.FindGameObjectWithTag("Llave 3");
+        Llave4 = GameObject.FindGameObjectWithTag("Llave 4");
+        Luz = GameObject.FindGameObjectWithTag("Luz");
         puertaLlave = GameObject.FindGameObjectWithTag("Puerta Llave");
         puertaLlave2 = GameObject.FindGameObjectWithTag("Puerta Llave2");
         puertaLlave3 = GameObject.FindGameObjectWithTag("Puerta Llave 3");
+        puertaLlave4 = GameObject.FindGameObjectWithTag("Puerta Llave 4");
         Trampilla = GameObject.FindGameObjectWithTag("Trampilla");
     }
 
@@ -48,6 +54,10 @@ public class GameControl : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (luz)
+        {
+
+        }
         checkDoor();
         checkKeyDoor();
         checkTrampilla();
@@ -69,6 +79,16 @@ public class GameControl : MonoBehaviour
         {
             llave = true;
             Destroy(Llave3);
+        }
+        else if (collision.gameObject.tag == "Llave 4")
+        {
+            llave = true;
+            Destroy(Llave4);
+        }
+        else if (collision.gameObject.tag == "Luz")
+        {
+            luz = true;
+            Destroy(Luz);
         }
     }
 
@@ -100,17 +120,37 @@ public class GameControl : MonoBehaviour
             transformJugador.localPosition = new Vector2(xProva3, yProva3); // anar a habitacio 3
         }
 
+        if (Input.GetKeyDown(KeyCode.E) && (transformJugador.localPosition.x >= xPos15 && transformJugador.localPosition.x <= xPos16) && (transformJugador.localPosition.y >= yPos15 && transformJugador.localPosition.y <= yPos16))
+        {
+            moviment getVariables = GetComponent<moviment>();
+            lastPosX = getVariables.transformJugador.localPosition.x;
+            lastPosY = getVariables.transformJugador.localPosition.y;
+            transformJugador.localPosition = new Vector2(xProva4, yProva4); // anar a habitacio 4
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && (transformJugador.localPosition.x >= xPos19 && transformJugador.localPosition.x <= xPos20) && (transformJugador.localPosition.y >= yPos19 && transformJugador.localPosition.y <= yPos20))
+        {
+            moviment getVariables = GetComponent<moviment>();
+            lastPosX = getVariables.transformJugador.localPosition.x;
+            lastPosY = getVariables.transformJugador.localPosition.y;
+            transformJugador.localPosition = new Vector2(xProva5, yProva5); // anar a habitacio 5
+        }
+
         if (Input.GetKeyDown(KeyCode.E) && (transformJugador.localPosition.x >= xPos5 && transformJugador.localPosition.x <= xPos6) && (transformJugador.localPosition.y >= yPos5 && transformJugador.localPosition.y <= yPos6))
         {
-            transformJugador.localPosition = new Vector2(lastPosX, lastPosY); // tornar al nivell 1
+            transformJugador.localPosition = new Vector2(lastPosX, lastPosY + 5); // tornar al nivell 1
             habitacio1 = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.E) && (transformJugador.localPosition.x >= xPos7 && transformJugador.localPosition.x <= xPos8) && (transformJugador.localPosition.y >= yPos7 && transformJugador.localPosition.y <= yPos8)) transformJugador.localPosition = new Vector2(lastPosX, lastPosY);
+        if (Input.GetKeyDown(KeyCode.E) && (transformJugador.localPosition.x >= xPos7 && transformJugador.localPosition.x <= xPos8) && (transformJugador.localPosition.y >= yPos7 && transformJugador.localPosition.y <= yPos8)) transformJugador.localPosition = new Vector2(lastPosX, lastPosY + 5);
 
-        if (Input.GetKeyDown(KeyCode.E) && (transformJugador.localPosition.x >= xPos9 && transformJugador.localPosition.x <= xPos10) && (transformJugador.localPosition.y >= yPos9 && transformJugador.localPosition.y <= yPos10)) transformJugador.localPosition = new Vector2(nivel2X, nivel2Y);
+        if (Input.GetKeyDown(KeyCode.E) && (transformJugador.localPosition.x >= xPos9 && transformJugador.localPosition.x <= xPos10) && (transformJugador.localPosition.y >= yPos9 && transformJugador.localPosition.y <= yPos10)) transformJugador.localPosition = new Vector2(nivel2X, nivel2Y + 5);
 
-        if (Input.GetKeyDown(KeyCode.E) && (transformJugador.localPosition.x >= xPos13 && transformJugador.localPosition.x <= xPos14) && (transformJugador.localPosition.y >= yPos13 && transformJugador.localPosition.y <= yPos14)) transformJugador.localPosition = new Vector2(lastPosX, lastPosY);
+        if (Input.GetKeyDown(KeyCode.E) && (transformJugador.localPosition.x >= xPos13 && transformJugador.localPosition.x <= xPos14) && (transformJugador.localPosition.y >= yPos13 && transformJugador.localPosition.y <= yPos14)) transformJugador.localPosition = new Vector2(lastPosX, lastPosY + 5);
+
+        if (Input.GetKeyDown(KeyCode.E) && (transformJugador.localPosition.x >= xPos17 && transformJugador.localPosition.x <= xPos18) && (transformJugador.localPosition.y >= yPos17 && transformJugador.localPosition.y <= yPos18)) transformJugador.localPosition = new Vector2(lastPosX, lastPosY + 5);
+
+        if (Input.GetKeyDown(KeyCode.E) && (transformJugador.localPosition.x >= xPos21 && transformJugador.localPosition.x <= xPos22) && (transformJugador.localPosition.y >= yPos21 && transformJugador.localPosition.y <= yPos22)) transformJugador.localPosition = new Vector2(lastPosX, lastPosY + 5);
 
     }
     
@@ -139,6 +179,15 @@ public class GameControl : MonoBehaviour
             if (llave && Input.GetKey(KeyCode.E))
             {
                 Destroy(puertaLlave3);
+                llave = false;
+            }
+        }
+
+        if ((transformJugador.localPosition.x >= x1PL4 && transformJugador.localPosition.x <= x2PL4) && (transformJugador.localPosition.y >= y1PL4 && transformJugador.localPosition.y <= y2PL4))
+        {
+            if (llave && Input.GetKey(KeyCode.E))
+            {
+                Destroy(puertaLlave4);
                 llave = false;
             }
         }
