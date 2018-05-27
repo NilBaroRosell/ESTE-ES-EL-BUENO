792cee1e3211ae0b72d9283d1079a2a4af92f8a7
Lluís <1999lluis@gmail.com>
2018-05-28 00:16:35 +0200

    Inicio menús y vida retocada

---
+++ Mindblow/Assets/DeathMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu : MonoBehaviour
+{
+
+    public void Quit()
+    {
+        Debug.Log("APPLICATION QUIT");
+        Application.Quit();
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene("prova");
+    }
+}
