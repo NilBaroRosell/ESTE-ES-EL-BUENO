8ff2f079e8a1a60b7d2f7c8f1761512a4bdb4e65
Lluís <1999lluis@gmail.com>
2018-05-28 01:46:14 +0200

    Añandiendo cosas, probando que funcionen

---
+++ Mindblow/Assets/VictoryMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryMenu : MonoBehaviour {
+
+    public void Quit()
+    {
+        Debug.Log("APPLICATION QUIT");
+        Application.Quit();
+    }
+}
